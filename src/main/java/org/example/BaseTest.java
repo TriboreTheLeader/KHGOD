1e88ad6ebb4034a0969319bfb87cfc286711abf6
TriboreTheLeader <haaszpeter2003@gmail.com>
2024-08-23 08:31:35 +0200

    KHGOD V.02 Better Structure

--- src/main/java/org/example/BaseTest.java
+++ src/main/java/org/example/BaseTest.java
@@ -1,20 +1,17 @@
 package org.example;
 
-import org.example.Xpaths.Xpaths;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-import java.util.Map;
-import java.util.logging.Level;
+
 
 public class BaseTest {
 
-    protected WebDriver driver;
+    public WebDriver driver;
 
     @BeforeMethod
     public void setUp() {
