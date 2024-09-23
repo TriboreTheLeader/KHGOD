523b61d50e1b6ee93a0c1bf9575de2644b047701
TriboreTheLeader <haaszpeter2003@gmail.com>
2024-09-23 11:22:00 +0200

    KHGOD V0.3

--- src/main/java/org/example/KHsteps/KHV1LOGIN.java
+++ src/main/java/org/example/KHsteps/KHV1LOGIN.java
@@ -2,46 +2,38 @@ package org.example.KHsteps;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.example.BaseTest;
 import org.example.Procedures.Procedures;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 import java.awt.*;
-import java.io.BufferedReader;
-import java.io.FileReader;
+
 import java.io.IOException;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-
-import static org.example.Xpaths.Xpaths.*;
 
 public class KHV1LOGIN extends BaseTest {
 
-    @Test
-    public void
 
-    accessSiteWithCookies() throws InterruptedException, IOException, AWTException {
-        int count = 1; // Számláló kezdeti értéke
+    @Test
+    public void accessSiteWithCookies() throws InterruptedException, IOException, AWTException {
+        int count = 1;
 
         while (true) {
             try {
-
                 WebDriverManager.chromedriver().setup();
-
-                // Create ChromeOptions instance and add arguments to disable first-run UI
                 ChromeOptions options = new ChromeOptions();
                 options.addArguments("--disable-search-engine-choice-screen");
                 options.addArguments("--disable-popup-blocking");
-
-
-
-                // Initialize WebDriver with ChromeOptions
                 driver = new ChromeDriver(options);
-
-                System.out.println("Ciklus száma: " + count); // Logolás a konzolra
-
+                System.out.println("Ciklus száma: " + count);
                 driver.get("https://klanhaboru.hu");
+                if (isBotProtectionVisible()) {
+                    System.out.println("Bot védelem észlelve, a teszt leáll.");
+                    break;
+                }
                 Procedures procedures = new Procedures(driver);
                 procedures.LOGIN();
                 procedures.Farm();
@@ -50,19 +42,25 @@ public class KHV1LOGIN extends BaseTest {
                     driver.quit();
                 }
             }
-
-
             Thread.sleep(150000);
-
-            count++; // Növeld a számlálót minden ciklus után
+            count++;
         }
     }
-
-
-
-
-
+    private boolean isBotProtectionVisible() {
+        try {
+            // Locate the bot protection element by its id
+            WebElement botProtectionElement = driver.findElement(By.id("botprotection_quest"));
+            return botProtectionElement.isDisplayed(); // Returns true if the element is visible
+        } catch (Exception e) {
+            return false; // If element is not found or not visible, return false
+        }
+    }
 }
+
+
+
+
+
 
 
 
